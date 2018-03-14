25fe63506a2952a44566426c91584bc365851475
Domantas Grotuzas <DomantasGrotuzas@gmail.com>
2018-03-14 23:55:08 +0200

    First commit. Connect to mail.stud.vu.lt

---
+++ POP3/POP3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POP3
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            String ip = "193.219.80.134";
+            IPAddress ipAddress = IPAddress.Parse(ip);
+            IPEndPoint remoteEP = new IPEndPoint(ipAddress, 110);
+            Socket socket = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                socket.Connect(remoteEP);
+                Console.WriteLine("Socket connected to {0}", socket.RemoteEndPoint.ToString());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.StackTrace);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
